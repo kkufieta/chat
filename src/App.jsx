3f06eb64204aab1b1c86588abcfcb6204ef88c51
kkufieta <katharina.kufieta@gmail.com>
2020-09-05 16:10:00 -0400

    Change *.js to *.jsx files, extract Header into component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React, {Component} from 'react';
+
+import Header from './Header.jsx';
 
 /*
 This exercise will help you practice many of your newly aquired React skills.
@@ -8,12 +10,12 @@ This exercise will help you practice many of your newly aquired React skills.
 The instructions are included in the `instructions.md` file.
 */
 
-const users = [{ username: 'Amy' }, { username: 'John' }];
+const users = [{username: 'Amy'}, {username: 'John'}];
 
 const messages = [
-  { username: 'Amy', text: 'Hi, Jon!' },
-  { username: 'Amy', text: 'How are you?' },
-  { username: 'John', text: 'Hi, Amy! Good, you?' },
+  {username: 'Amy', text: 'Hi, Jon!'},
+  {username: 'Amy', text: 'How are you?'},
+  {username: 'John', text: 'Hi, Amy! Good, you?'},
 ];
 
 class App extends Component {
@@ -27,17 +29,14 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
-        </header>
-        <div className="container">
-          <div className="chat-window">
+      <div className='App'>
+        <Header />
+        <div className='container'>
+          <div className='chat-window'>
             <h2>Super Awesome Chat</h2>
             <div className="name sender">{users[0].username}</div>
 
-            <ul className="message-list">
+            <ul className='message-list'>
               {messages.map((message, index) => (
                 <li
                   key={index}
@@ -53,8 +52,8 @@ class App extends Component {
             <div>
               <form className="input-group">
                 <input type="text" className="form-control" placeholder="Enter your message..." />
-                <div className="input-group-append">
-                  <button className="btn submit-button" disabled={this.isDisabled()}>
+                <div className='input-group-append'>
+                  <button className='btn submit-button' disabled={this.isDisabled()}>
                     SEND
                   </button>
                 </div>
@@ -64,7 +63,7 @@ class App extends Component {
 
           <div className="chat-window">
             <h2>Super Awesome Chat</h2>
-            <div className="name sender">{users[1].username}</div>
+            <div className='name sender'>{users[1].username}</div>
             <ul className="message-list">
               {messages.map((message, index) => (
                 <li
@@ -79,10 +78,10 @@ class App extends Component {
             </ul>
 
             <div>
-              <form className="input-group">
-                <input type="text" className="form-control" placeholder="Enter your message..." />
-                <div className="input-group-append">
-                  <button className="btn submit-button" disabled={this.isDisabled()}>
+              <form className='input-group'>
+                <input type='text' className='form-control' placeholder='Enter your message...' />
+                <div className='input-group-append'>
+                  <button className='btn submit-button' disabled={this.isDisabled()}>
                     SEND
                   </button>
                 </div>
